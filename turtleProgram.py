f24263c74f86da23923646d45017357dad4c7d78
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 13:39:14 +0530

    changes made to turtle program

---
+++ turtleProgram.py
@@ -0,0 +1,6 @@
+import turtle
+wn =  turtle.Screen() #wn is an instance of the screen class
+alex = turtle.Turtle() #alex is an instance of the turtles class
+alex.forward(150) # forward and left are the method
+alex.left(90)
+alex.forward(75)
