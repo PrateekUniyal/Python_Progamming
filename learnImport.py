410c4f05e5146d02ad185171bd515cce4f1351de
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 14:27:47 +0530

    changes made to learnImport

--- learnImport.py
+++ learnImport.py
@@ -3,5 +3,5 @@ import random
 prob = random.random()
 print(prob)
 
-dice_throw = random.randrange(1,7)
+dice_throw = random.randrange(1,7) #give me an integer from 1,2,3,4,5,6 but not 7
 print(dice_throw)
