bcad3b22ef7758270c5042b4f5df364a0efc8b3c
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 14:00:58 +0530

    changes made to for _loop program

---
+++ for_looping.py
@@ -0,0 +1,7 @@
+print("This will be executed first")
+
+for _ in range(3):
+    print("This will be printed 3 times")
+    print("This will also be executed 3 times")
+
+print("We are now outside the loop")
