28039d59a4ee4a1ac3450b115cbb0c89439b0c56
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 14:12:15 +0530

    changes made to for _loop program

--- for_looping.py
+++ for_looping.py
@@ -1,5 +1,5 @@
 #For loop in python
-#---------------------------------------------
+
 print("This will be executed first")
 
 for _ in range(3):
@@ -7,3 +7,13 @@ for _ in range(3):
     print("This will also be executed 3 times")
 
 print("We are now outside the loop")
+
+import turtle
+wn = turtle.Screen()
+elan = turtle.Turtle()
+distance = 50
+for _ in range(10):
+    elan.forward(distance)
+    elan.right(90)
+    distance = distance + 10
+
