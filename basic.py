918e8599825b83018477d52d24d90385b1539d65
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 11:28:05 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,33 @@
+# print(100)
+# print("Hello World!")
+# print(3.14)
+# print(10/3)
+# print(10//3)
+# print(4*2)
+# print(4 ** 2)
+# #printing in python
+# print(type("Hello World"))
+# print(type(100))
+# print(type(3.14))
+# #type conversion
+# print((3.14), int(3.14))
+# print((10.28), float(12))
+# print((3.14), print(type(str(3.14))))
+
+#------------------------------------------------------------------------------
+
+message = "Hello There"
+n = 17
+pi = 3.14159
+
+print(message)
+print(n)
+print(pi)
+
+x = 2
+print(x)
+x = 10
+print(x)
+
+this_is_variable = "This value is inside a variable having underscore"
+print(this_is_variable)
