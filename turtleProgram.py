f722a5d0be64b48995ce44181f00ab111f0569e2
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 13:41:41 +0530

    changes made to turtle program

--- turtleProgram.py
+++ turtleProgram.py
@@ -5,3 +5,5 @@ alex = turtle.Turtle() #alex is an instance of the turtles class
 alex.forward(150) # forward and left are the method
 alex.left(90)
 alex.forward(75)
+alex.salary = 50000
+print(alex.salary)
