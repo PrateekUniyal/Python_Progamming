b9e61ace4ce25246b94561d0685f8b9524dd844b
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 13:49:45 +0530

    changes made to turtle program

--- turtleProgram.py
+++ turtleProgram.py
@@ -2,8 +2,22 @@
 import turtle
 wn =  turtle.Screen() #wn is an instance of the screen class
 alex = turtle.Turtle() #alex is an instance of the turtles class
+alex.color("red")
+tess = turtle.Turtle()
+tess.pensize(5)
 alex.forward(150) # forward and left are the method
 alex.left(90)
 alex.forward(75)
-alex.salary = 50000
-print(alex.salary)
+alex.left(90)
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+# alex.salary = 50000
+print("Alex made a rectangle")
