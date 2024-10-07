59d8c080df72bf5d3c5a0abe959634341f1baea3
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 15:46:17 +0530

    changes made to string list and tuple programs

---
+++ string_list_tuple_programs.py
@@ -0,0 +1,21 @@
+# a sequence is an ordered collection
+s = "Hello World"
+print(len(s))
+s = """This is a multi line string"""
+print(s)
+
+# A string is a sequence of characters
+
+# list is a sequence of any type of value
+
+myList = ["one" , 2, 'three']
+print(myList)
+
+# tuples are immutable that means once created it cant be changed
+myTuple = ("one", 2 ,'three')
+my_tuple = (100)
+print(type(myList))
+print(type(myTuple))
+print(type(my_tuple))
+my_tuple = (100,)
+print(type(my_tuple))
