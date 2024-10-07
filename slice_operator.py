423f2b031f78a04c744de09f8d023d0cee91eb38
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 16:42:00 +0530

    changes made to string list and tuple programs

---
+++ slice_operator.py
@@ -0,0 +1,12 @@
+# slicing is used to get a subsequence of items
+players = "a,b,c,d,e"
+print(len(players))
+print(players[0:9])
+print((players[4:7]))
+s = "ABCDE"
+print(len(s))
+print(s[2:4])
+print(s[:]) #copy of the sequence
+s = "python rocks"
+print(s[3:8])
+
