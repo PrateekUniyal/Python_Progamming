60d42095dbb87946f8616b737ebedc12803296fa
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 14:21:50 +0530

    changes made to for _loop program

--- for_looping.py
+++ for_looping.py
@@ -11,9 +11,11 @@ print("We are now outside the loop")
 import turtle
 wn = turtle.Screen()
 elan = turtle.Turtle()
+elan.shape("turtle")
 distance = 50
-for _ in range(30):
+for _ in range(20):
     elan.forward(distance)
     elan.right(90)
     distance = distance + 10
+
 
