c0a777934c625711ddc81104f1cf7b45ff13061f
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-07 17:49:32 +0530

    changes made to conactenation repeatation split file

---
+++ conactenation_repeatation.py
@@ -0,0 +1,8 @@
+fruit = ["apple", "mango", "orange", "kiwi"]
+print(fruit * 4) # the items of the list are printed 4 times
+s = "Python is fun"
+print(s.count('n')) # count the number of occurence of a  character in a string
+print(s.index("n"))
+song = "The rain in Spain"
+wds = song.split()
+print(wds)
