c027f23afb521ac3e9d798ae6a1ff017ea81ecff
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-08 10:32:31 +0530

    changes made to iteration programs

--- iteration_programs.py
+++ iteration_programs.py
@@ -2,3 +2,16 @@
 mylist = ["one", "two", "three", "four", "five"]
 for color in ["red", "blue", "green", "yellow", "white", "black"]: # color is the loop variable
     print("The color present in the sequence is ",color)
+for achar in "Hello world":
+    print(achar)
+
+import turtle
+wn = turtle.Screen()
+alex = turtle.Turtle()
+for acolor in ["Yellow", "Red", "Green","Black"]:
+    alex.color(acolor)
+    alex.forward(50)
+    alex.left(90)
+
+wn.exitonclick()
+
