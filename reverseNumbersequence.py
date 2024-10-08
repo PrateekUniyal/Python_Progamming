8ffc037a817ca8c9a0ccf78f6d80d006b94f1de0
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-08 12:44:16 +0530

    new file containing code to printthe reverse sequence

---
+++ reverseNumbersequence.py
@@ -0,0 +1,5 @@
+# Program to print the reverse sequence of numbers from n to 1
+
+n =  int(input("Please enter the number to print the reverse sequence: "))
+for i in range(n,0,-1): #  i is the loop variable o n is the start,0 is stop ,-1 is the steps
+    print(i)
