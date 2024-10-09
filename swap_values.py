5dfd95023495e7d5de610ef4663d09a5f941805a
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-09 15:15:51 +0530

    added program to swap two numbers

---
+++ swap_values.py
@@ -0,0 +1,10 @@
+# Python program to swap values of two variables
+a = int(input("Please enter the first value: "))
+b = int(input("Please enter the second value: "))
+print("The a is: ",a)
+print("The b is: ",b)
+c = a
+a = b
+b = c
+print("After swapping a is: ",a)
+print("After swapping b is: ",b)
