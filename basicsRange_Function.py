9377f4e2f1df9aa4f864ff262c891e83d4991bf2
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-09 10:50:00 +0530

    changes made to the basicsRange function program

---
+++ basicsRange_Function.py
@@ -0,0 +1,15 @@
+# Basics of range function
+
+n = int(input("Please enter the range: "))
+for i in range(n):
+    print(i)
+print("------------------")
+for i in range(n+1):
+    print(i)
+print("------------------")
+for i in range(1,n+1):
+    print(i)
+print("------------------")
+for i in range(-5,n+1):
+    print(i)
+print("------------------")
