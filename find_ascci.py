da9852c252f531890722967b5952b08b5fbf3b3a
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-11 12:56:01 +0530

    added a code to find ascii value and made changes to star pattern code

---
+++ find_ascci.py
@@ -0,0 +1,3 @@
+# program to find the ascii value
+ch = input("Please enter the character to find the ascii value:")
+print("The ASCII value of ",ch,"is",ord(ch))
--- star_pattern.py
+++ star_pattern.py
@@ -9,7 +9,6 @@
 for i in range(1, 6 ):
     print('*' * i)
 
-
 print("Below is Inverted triangle.")
 
 for j in range(5,0,-1):
