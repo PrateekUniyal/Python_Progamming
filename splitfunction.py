bee3a1ab03376202083ed8bf57ce464af7df1a16
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-15 10:27:55 +0530

    Added a code to understand methods in list

---
+++ splitfunction.py
@@ -0,0 +1,16 @@
+# Program to practice split function
+numbers = "1 2 3 4 5"
+new_list = [int(num) for num in numbers.split()]
+message = "This program is made to understand split function better"
+new_message = message.split()
+numbers1 = "12345678"
+new_numbers1 = numbers1.split()
+num = [int(digits) for digits in new_numbers1]
+print("The length of num is :",len(num))
+print("The length of new_numbers1 is: ", len(new_numbers1))
+print("The type of num is :",type(num))
+print("The type of new_numbers1 is: ", type(new_numbers1))
+print(num)
+print(new_message)
+print(new_list)
+print(new_numbers1)
