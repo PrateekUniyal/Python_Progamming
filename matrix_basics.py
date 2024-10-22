c58b6a2d4d23d603cf5c8252b9163fa4d8ba1d33
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-22 13:23:13 +0530

    Added basic matrix program

--- matrix_basics.py
+++ matrix_basics.py
@@ -1,52 +1,13 @@
-# program to create a spiral box
-row = 8
-col = 5
+# program to learn matrix operations
 
-# array = [
-#     [1,2,3],
-#     [4,5,6],
-#     [7,8,9]
-# ]
-#
-# for row in array:
-#     print(row)
-#
-# for row in array:
-#     for element in row:
-#         print(element)
-#     print()
-arr = [[0 for ele in range(col)] for r in range(row)]
-# arr = [[0 for r in range(row)] for ele in range(col)]
-for row in arr:
+row =2
+col =2
+
+mat = [[0 for _ in range(row)] for _ in range(col)]
+for row in mat:
     print(row)
-print("********************************************************")
-
-for i in range(len(arr)):
-    for j in range(len(arr[0])):
-        if i==0 or i==len(arr)-1:
-            arr[i][j] = 1
-        if j ==0 or j == len(arr)-1:
-            arr[i][j] = 1
-        if i == len(arr)-1 or i == 0:
-            arr[i][j] = 1
-        if j == len(arr)-1 or i == 0:
-            arr[i][j] = 1
-
-for row in arr:
+mat[0][0] = 1
+mat[col-1][col-1] = 1
+print("*********************")
+for row in mat:
     print(row)
-print()
-print("*********************************************************************")
-print()
-# array = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
-#
-# for row in array:
-#     for item in row:
-#         print(item, end=" ")
-#     print()
-
-
-
