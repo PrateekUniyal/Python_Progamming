09fca6d27827ba36fb54729488265cf54ea5f430
Prateek Uniyal <uniyalprateek13@gmail.com>
2024-10-23 14:32:17 +0530

    Added programs to find animals in farm through given count of legs,heads,horns.

--- totalAnimalCount.py
+++ totalAnimalCount.py
@@ -5,11 +5,6 @@ def get_animals_count(legs_number, heads_number, horns_number):
 #     totalanimals = heads_number # total animals
     remaining_head = heads_number - cows # getting the number of rabbits and chicken
     remaininglegs = legs_number - cows * 4 # getting legs of rabbits and chickens
-    rabbits = 0
-#     while sum != totallegs:
-#         sum = (rabbits * 4) + (chicken * 2)
-#         rabbits += 1
-#         chicken -= 1
     rabbits = (remaininglegs // 2) - remaining_head
     chicken = remaining_head -rabbits
     ans = {
